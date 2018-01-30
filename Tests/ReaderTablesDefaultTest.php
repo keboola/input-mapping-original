7fa12f153e833f1aa125fdfec8fac262c8d7ecfe
Odin <odinuv@gmail.com>
2018-01-30 14:54:05 +0100

    tests: extend test data

--- Tests/ReaderTablesDefaultTest.php
+++ Tests/ReaderTablesDefaultTest.php
@@ -27,8 +27,10 @@ class ReaderTablesDefaultTest extends ReaderTablesTestAbstract
 
         // Create table
         $csv = new CsvFile($this->temp->getTmpFolder() . DIRECTORY_SEPARATOR . "upload.csv");
-        $csv->writeRow(["Id", "Name"]);
-        $csv->writeRow(["test", "test"]);
+        $csv->writeRow(["Id", "Name", "foo", "bar"]);
+        $csv->writeRow(["id1", "name1", "foo1", "bar1"]);
+        $csv->writeRow(["id2", "name2", "foo2", "bar2"]);
+        $csv->writeRow(["id3", "name3", "foo3", "bar3"]);
         $this->client->createTableAsync("in.c-docker-test", "test", $csv);
         $this->client->setTableAttribute("in.c-docker-test.test", "attr1", "val1");
     }
@@ -46,7 +48,8 @@ class ReaderTablesDefaultTest extends ReaderTablesTestAbstract
         $reader->downloadTables($configuration, $this->temp->getTmpFolder() . DIRECTORY_SEPARATOR . "download");
 
         self::assertEquals(
-            "\"Id\",\"Name\"\n\"test\",\"test\"\n",
+            "\"Id\",\"Name\",\"foo\",\"bar\"\n\"id1\",\"name1\",\"foo1\",\"bar1\"\n" .
+             "\"id2\",\"name2\",\"foo2\",\"bar2\"\n\"id3\",\"name3\",\"foo3\",\"bar3\"\n",
             file_get_contents($this->temp->getTmpFolder() . "/download/test.csv")
         );
 
@@ -69,7 +72,8 @@ class ReaderTablesDefaultTest extends ReaderTablesTestAbstract
 
         $reader->downloadTables($configuration, $this->temp->getTmpFolder() . DIRECTORY_SEPARATOR . "download");
         self::assertEquals(
-            "\"Id\",\"Name\"\n\"test\",\"test\"\n",
+            "\"Id\",\"Name\",\"foo\",\"bar\"\n\"id1\",\"name1\",\"foo1\",\"bar1\"\n" .
+            "\"id2\",\"name2\",\"foo2\",\"bar2\"\n\"id3\",\"name3\",\"foo3\",\"bar3\"\n",
             file_get_contents($this->temp->getTmpFolder() . DIRECTORY_SEPARATOR . "download/test.csv")
         );
     }
@@ -87,7 +91,8 @@ class ReaderTablesDefaultTest extends ReaderTablesTestAbstract
 
         $reader->downloadTables($configuration, $this->temp->getTmpFolder() . DIRECTORY_SEPARATOR . "download");
         self::assertEquals(
-            "\"Id\",\"Name\"\n\"test\",\"test\"\n",
+            "\"Id\",\"Name\",\"foo\",\"bar\"\n\"id1\",\"name1\",\"foo1\",\"bar1\"\n" .
+            "\"id2\",\"name2\",\"foo2\",\"bar2\"\n\"id3\",\"name3\",\"foo3\",\"bar3\"\n",
             file_get_contents($this->temp->getTmpFolder() . DIRECTORY_SEPARATOR . "download/test.csv")
         );
     }
@@ -166,7 +171,7 @@ class ReaderTablesDefaultTest extends ReaderTablesTestAbstract
         self::assertEquals('bar', $manifest['metadata'][0]['value']);
         self::assertEquals('fooBar', $manifest['metadata'][1]['key']);
         self::assertEquals('baz', $manifest['metadata'][1]['value']);
-        self::assertCount(2, $manifest['column_metadata']);
+        self::assertCount(4, $manifest['column_metadata']);
         self::assertArrayHasKey('Id', $manifest['column_metadata']);
         self::assertArrayHasKey('Name', $manifest['column_metadata']);
         self::assertCount(0, $manifest['column_metadata']['Id']);
@@ -179,6 +184,75 @@ class ReaderTablesDefaultTest extends ReaderTablesTestAbstract
         self::assertEquals('someKey', $manifest['column_metadata']['Name'][0]['key']);
         self::assertEquals('someValue', $manifest['column_metadata']['Name'][0]['value']);
     }
+
+    public function testReadTableColumns()
+    {
+        $tableMetadata = [
+            [
+                'key' => 'foo',
+                'value' => 'bar'
+            ],
+            [
+                'key' => 'fooBar',
+                'value' => 'baz'
+            ]
+        ];
+        $columnMetadata = [
+            [
+                'key' => 'someKey',
+                'value' => 'someValue'
+            ]
+        ];
+        $metadata = new Metadata($this->client);
+        $metadata->postTableMetadata('in.c-docker-test.test', 'dataLoaderTest', $tableMetadata);
+        $metadata->postColumnMetadata('in.c-docker-test.test.Name', 'dataLoaderTest', $columnMetadata);
+        $reader = new Reader($this->client, new NullLogger());
+        $configuration = [
+            [
+                "source" => "in.c-docker-test.test",
+                "destination" => "test.csv",
+            ]
+        ];
+
+        $reader->downloadTables($configuration, $this->temp->getTmpFolder() . DIRECTORY_SEPARATOR . "download", "s3");
+
+        $adapter = new Adapter();
+
+        $manifest = $adapter->readFromFile($this->temp->getTmpFolder() . "/download/test.csv.manifest");
+        self::assertEquals("in.c-docker-test.test", $manifest["id"]);
+        self::assertEquals("val1", $manifest["attributes"][0]["value"]);
+        $this->assertS3info($manifest);
+        self::assertArrayHasKey('metadata', $manifest);
+        self::assertCount(2, $manifest['metadata']);
+        self::assertArrayHasKey('id', $manifest['metadata'][0]);
+        self::assertArrayHasKey('key', $manifest['metadata'][0]);
+        self::assertArrayHasKey('value', $manifest['metadata'][0]);
+        self::assertArrayHasKey('provider', $manifest['metadata'][0]);
+        self::assertArrayHasKey('timestamp', $manifest['metadata'][0]);
+        self::assertArrayHasKey('id', $manifest['metadata'][1]);
+        self::assertArrayHasKey('key', $manifest['metadata'][1]);
+        self::assertArrayHasKey('value', $manifest['metadata'][1]);
+        self::assertArrayHasKey('provider', $manifest['metadata'][1]);
+        self::assertArrayHasKey('timestamp', $manifest['metadata'][1]);
+        self::assertEquals('dataLoaderTest', $manifest['metadata'][0]['provider']);
+        self::assertEquals('foo', $manifest['metadata'][0]['key']);
+        self::assertEquals('bar', $manifest['metadata'][0]['value']);
+        self::assertEquals('fooBar', $manifest['metadata'][1]['key']);
+        self::assertEquals('baz', $manifest['metadata'][1]['value']);
+        self::assertCount(4, $manifest['column_metadata']);
+        self::assertArrayHasKey('Id', $manifest['column_metadata']);
+        self::assertArrayHasKey('Name', $manifest['column_metadata']);
+        self::assertCount(0, $manifest['column_metadata']['Id']);
+        self::assertCount(1, $manifest['column_metadata']['Name']);
+        self::assertArrayHasKey('id', $manifest['column_metadata']['Name'][0]);
+        self::assertArrayHasKey('key', $manifest['column_metadata']['Name'][0]);
+        self::assertArrayHasKey('value', $manifest['column_metadata']['Name'][0]);
+        self::assertArrayHasKey('provider', $manifest['column_metadata']['Name'][0]);
+        self::assertArrayHasKey('timestamp', $manifest['column_metadata']['Name'][0]);
+        self::assertEquals('someKey', $manifest['column_metadata']['Name'][0]['key']);
+        self::assertEquals('someValue', $manifest['column_metadata']['Name'][0]['value']);
+    }
+
 
     public function testChangedSinceAndDaysConfiguration()
     {
@@ -194,9 +268,9 @@ class ReaderTablesDefaultTest extends ReaderTablesTestAbstract
 
         try {
             $reader->downloadTables($configuration, $this->temp->getTmpFolder() . DIRECTORY_SEPARATOR . "download");
-            $this->fail("Exception not caught");
+            self::fail("Exception not caught");
         } catch (InvalidInputException $e) {
-            $this->assertEquals("Cannot set both parameters 'days' and 'changed_since'.", $e->getMessage());
+            self::assertEquals("Cannot set both parameters 'days' and 'changed_since'.", $e->getMessage());
         }
     }
 }
