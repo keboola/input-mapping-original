7a9c80dd28febcef97018bac2193daafa74377f9
Martin Zajic <zajca@zajca.cz>
2019-09-24 15:43:41 +0200

    use dataProvider in TableConfigurationTest

--- Tests/Configuration/TableConfigurationTest.php
+++ Tests/Configuration/TableConfigurationTest.php
@@ -3,121 +3,178 @@
 namespace Keboola\InputMapping\Tests\Configuration;
 
 use Keboola\InputMapping\Configuration\Table;
+use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
 
 class TableConfigurationTest extends \PHPUnit_Framework_TestCase
 {
-    public function testBasicConfiguration()
+    /**
+     * @return mixed[][]
+     */
+    public function provideValidConfigs()
     {
-        $config = [
-            "source" => "in.c-main.test"
+        return [
+            'BasicConfiguration' => [
+                [
+                    "source" => "in.c-main.test",
+                ],
+                [
+                    "source" => "in.c-main.test",
+                    "columns" => [],
+                    "where_values" => [],
+                    "where_operator" => "eq",
+                ],
+            ],
+            'ComplexConfiguration' => [
+                [
+                    "source" => "in.c-main.test",
+                    "destination" => "test",
+                    "changed_since" => "-1 days",
+                    "columns" => ["Id", "Name"],
+                    "where_column" => "status",
+                    "where_values" => ["val1", "val2"],
+                    "where_operator" => "ne",
+                ],
+                [
+                    "source" => "in.c-main.test",
+                    "destination" => "test",
+                    "changed_since" => "-1 days",
+                    "columns" => ["Id", "Name"],
+                    "where_column" => "status",
+                    "where_values" => ["val1", "val2"],
+                    "where_operator" => "ne",
+                ],
+            ],
+            'DaysNullConfiguration' => [
+                [
+                    "source" => "in.c-main.test",
+                    "destination" => "test",
+                    "days" => null,
+                    "columns" => ["Id", "Name"],
+                    "where_column" => "status",
+                    "where_values" => ["val1", "val2"],
+                    "where_operator" => "ne",
+                ],
+                [
+                    "source" => "in.c-main.test",
+                    "destination" => "test",
+                    "days" => null,
+                    "columns" => ["Id", "Name"],
+                    "where_column" => "status",
+                    "where_values" => ["val1", "val2"],
+                    "where_operator" => "ne",
+                ],
+            ],
+            'DaysConfiguration' => [
+                [
+                    "source" => "in.c-main.test",
+                    "destination" => "test",
+                    "days" => 1,
+                    "columns" => ["Id", "Name"],
+                    "where_column" => "status",
+                    "where_values" => ["val1", "val2"],
+                    "where_operator" => "ne",
+                ],
+                [
+                    "source" => "in.c-main.test",
+                    "destination" => "test",
+                    "days" => 1,
+                    "columns" => ["Id", "Name"],
+                    "where_column" => "status",
+                    "where_values" => ["val1", "val2"],
+                    "where_operator" => "ne",
+                ],
+            ],
+            'ChangedSinceNullConfiguration' => [
+                [
+                    "source" => "in.c-main.test",
+                    "destination" => "test",
+                    "changed_since" => null,
+                    "columns" => ["Id", "Name"],
+                    "where_column" => "status",
+                    "where_values" => ["val1", "val2"],
+                    "where_operator" => "ne",
+                ],
+                [
+                    "source" => "in.c-main.test",
+                    "destination" => "test",
+                    "changed_since" => null,
+                    "columns" => ["Id", "Name"],
+                    "where_column" => "status",
+                    "where_values" => ["val1", "val2"],
+                    "where_operator" => "ne",
+                ],
+            ],
+            'ChangedSinceConfiguration' => [
+                [
+                    "source" => "in.c-main.test",
+                    "destination" => "test",
+                    "changed_since" => "-1 days",
+                    "columns" => ["Id", "Name"],
+                    "where_column" => "status",
+                    "where_values" => ["val1", "val2"],
+                    "where_operator" => "ne",
+                ],
+                [
+                    "source" => "in.c-main.test",
+                    "destination" => "test",
+                    "changed_since" => "-1 days",
+                    "columns" => ["Id", "Name"],
+                    "where_column" => "status",
+                    "where_values" => ["val1", "val2"],
+                    "where_operator" => "ne",
+                ],
+            ],
         ];
-
-        $expectedArray = [
-            "source" => "in.c-main.test",
-            "columns" => [],
-            "where_values" => [],
-            "where_operator" => "eq"
-        ];
-        $processedConfiguration = (new Table())->parse(["config" => $config]);
-        self::assertEquals($expectedArray, $processedConfiguration);
-    }
-
-    public function testComplexConfiguration()
-    {
-        $config = [
-            "source" => "in.c-main.test",
-            "destination" => "test",
-            "changed_since" => "-1 days",
-            "columns" => ["Id", "Name"],
-            "where_column" => "status",
-            "where_values" => ["val1", "val2"],
-            "where_operator" => "ne"
-        ];
-
-        $expectedArray = $config;
-        $processedConfiguration = (new Table())->parse(["config" => $config]);
-        self::assertEquals($expectedArray, $processedConfiguration);
-    }
-
-    public function testDaysNullConfiguration()
-    {
-        $config = [
-            "source" => "in.c-main.test",
-            "destination" => "test",
-            "days" => null,
-            "columns" => ["Id", "Name"],
-            "where_column" => "status",
-            "where_values" => ["val1", "val2"],
-            "where_operator" => "ne"
-        ];
-
-        $expectedArray = $config;
-        $processedConfiguration = (new Table())->parse(["config" => $config]);
-        self::assertEquals($expectedArray, $processedConfiguration);
-    }
-
-    public function testDaysConfiguration()
-    {
-        $config = [
-            "source" => "in.c-main.test",
-            "destination" => "test",
-            "days" => 1,
-            "columns" => ["Id", "Name"],
-            "where_column" => "status",
-            "where_values" => ["val1", "val2"],
-            "where_operator" => "ne"
-        ];
-
-        $expectedArray = $config;
-        $processedConfiguration = (new Table())->parse(["config" => $config]);
-        self::assertEquals($expectedArray, $processedConfiguration);
-    }
-
-    public function testChangedSinceNullConfiguration()
-    {
-        $config = [
-            "source" => "in.c-main.test",
-            "destination" => "test",
-            "changed_since" => null,
-            "columns" => ["Id", "Name"],
-            "where_column" => "status",
-            "where_values" => ["val1", "val2"],
-            "where_operator" => "ne"
-        ];
-
-        $expectedArray = $config;
-        $processedConfiguration = (new Table())->parse(["config" => $config]);
-        self::assertEquals($expectedArray, $processedConfiguration);
-    }
-
-    public function testChangedSinceConfiguration()
-    {
-        $config = [
-            "source" => "in.c-main.test",
-            "destination" => "test",
-            "changed_since" => "-1 days",
-            "columns" => ["Id", "Name"],
-            "where_column" => "status",
-            "where_values" => ["val1", "val2"],
-            "where_operator" => "ne"
-        ];
-
-        $expectedArray = $config;
-        $processedConfiguration = (new Table())->parse(["config" => $config]);
-        self::assertEquals($expectedArray, $processedConfiguration);
     }
 
     /**
-     * @expectedException \Symfony\Component\Config\Definition\Exception\InvalidConfigurationException
-     * @expectedExceptionMessage Invalid configuration for path "table.where_operator": Invalid operator in where_operator "abc"
+     * @dataProvider provideValidConfigs
      */
-    public function testInvalidWhereOperator()
-    {
-        $config = [
-            "source" => "in.c-main.test",
-            "where_operator" => 'abc'
-        ];
+    public function testValidConfigDefinition(
+        array $config,
+        array $expected
+    ) {
+        $processedConfiguration = (new Table())->parse(["config" => $config]);
+        self::assertEquals($expected, $processedConfiguration);
+    }
 
+    /**
+     * @return mixed[][]
+     */
+    public function provideInvalidConfigs()
+    {
+        return [
+            'InvalidWhereOperator' => [
+                [
+                    "source" => "in.c-main.test",
+                    "where_operator" => 'abc',
+                ],
+                InvalidConfigurationException::class,
+                'Invalid configuration for path "table.where_operator": Invalid operator in where_operator "abc"',
+            ],
+            'testEmptyConfiguration' => [
+                [],
+                InvalidConfigurationException::class,
+                'The child node "source" at path "table" must be configured.',
+            ],
+            'EmptySourceConfiguration' => [
+                ["source" => ""],
+                InvalidConfigurationException::class,
+                'The path "table.source" cannot contain an empty value, but got "".',
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider provideInvalidConfigs
+     */
+    public function testInvalidConfigDefinition(
+        array $config,
+        $exception,
+        $exceptionMessage
+    ) {
+        $this->expectException($exception);
+        $this->expectExceptionMessage($exceptionMessage);
         (new Table())->parse(["config" => $config]);
     }
 
@@ -134,23 +191,5 @@ class TableConfigurationTest extends \PHPUnit_Framework_TestCase
         $expectedArray["where_values"] = [];
         $processedConfiguration = (new Table())->parse(["config" => $config]);
         self::assertEquals($expectedArray, $processedConfiguration);
-    }
-
-    /**
-     * @expectedException \Symfony\Component\Config\Definition\Exception\InvalidConfigurationException
-     * @expectedExceptionMessage The child node "source" at path "table" must be configured
-     */
-    public function testEmptyConfiguration()
-    {
-        (new Table())->parse(["config" => []]);
-    }
-
-    /**
-     * @expectedException \Symfony\Component\Config\Definition\Exception\InvalidConfigurationException
-     * @expectedExceptionMessage The path "table.source" cannot contain an empty value, but got "".
-     */
-    public function testEmptySourceConfiguration()
-    {
-        (new Table())->parse(["config" => ["source" => ""]]);
     }
 }
