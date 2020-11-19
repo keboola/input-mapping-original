2c592976a3208cbd2bea269f4044e69100ee2475
odin <odinuv@gmail.com>
2020-11-19 15:37:16 +0100

    fix: calling convention

--- Tests/Reader/DownloadTablesWorkspaceSynapseTest.php
+++ Tests/Reader/DownloadTablesWorkspaceSynapseTest.php
@@ -9,6 +9,7 @@ use Keboola\InputMapping\Reader\State\InputTableStateList;
 use Keboola\StorageApiBranch\ClientWrapper;
 use Keboola\StorageApi\Client;
 use Keboola\StorageApi\Exception;
+use Keboola\StorageApiBranch\ClientWrapper;
 use Psr\Log\Test\TestLogger;
 
 class DownloadTablesWorkspaceSynapseTest extends DownloadTablesWorkspaceTestAbstract
