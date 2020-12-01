4141c11499d29844c91d8d4db378f7871cdde01a
pivnicek <marc@keboola.com>
2020-12-01 12:43:10 +0100

    refactor ABS file strategy

--- Reader/Strategy/Files/ABSWorkspaceFilesStrategy.php
+++ Reader/Strategy/Files/ABSWorkspaceFilesStrategy.php
@@ -11,49 +11,25 @@ class ABSWorkspaceFilesStrategy extends AbstractFilesStrategy implements FilesSt
 {
     protected $workspaceType = WorkspaceProviderInterface::TYPE_ABS;
 
+    protected $inputs = [];
+
     public function downloadFile($fileInfo, $destinationPath)
     {
-        $workspaces = new Workspaces($this->clientWrapper->getBasicClient());
-        $workspaceId = $this->workspaceProvider->getWorkspaceId($this->workspaceType);
-        $workspaces->loadWorkspaceData($workspaceId, [
+        $this->inputs[] = [
             'dataFileId' => $fileInfo['id'],
             'destination' => $destinationPath,
-        ]);
+        ];
+        $this->manifestWriter->writeFileManifest($fileInfo, $destinationPath . ".manifest");
     }
 
     public function downloadFiles($fileConfigurations, $destination)
     {
-        $fileOptions = new GetFileOptions();
-        $fileOptions->setFederationToken(true);
-        $inputs = [];
-        foreach ($fileConfigurations as $fileConfiguration) {
-            $files = $this->getFiles($fileConfiguration);
-            foreach ($files as $file) {
-                $fileInfo = $this->clientWrapper->getBasicClient()->getFile($file['id'], $fileOptions);
-                $fileDestinationPath = sprintf('%s/%s_%s', $destination, $fileInfo['id'], $fileInfo["name"]);
-                $this->logger->info(sprintf('Fetching file %s (%s).', $fileInfo['name'], $file['id']));
-                $inputs[] = [
-                    'dataFileId' => $fileInfo['id'],
-                    'destination' => $fileDestinationPath,
-                ];
-                try {
-                    // add leading slash because we write the manifests locally
-                    $this->manifestWriter->writeFileManifest($fileInfo, '/' . $fileDestinationPath . ".manifest");
-                } catch (\Exception $e) {
-                    throw new InputOperationException(
-                        sprintf('Failed to write manifest file %s (%s).', $fileInfo['name'], $file['id']),
-                        0,
-                        $e
-                    );
-                }
-                $this->logger->info(sprintf('Prepared file %s (%s).', $fileInfo['name'], $file['id']));
-            }
-        }
-        if (!empty($inputs)) {
+        parent::downloadFiles($fileConfigurations, $destination);
+        if (!empty($this->inputs)) {
             $workspaces = new Workspaces($this->clientWrapper->getBasicClient());
             $workspaceId = $this->workspaceProvider->getWorkspaceId($this->workspaceType);
             $workspaces->loadWorkspaceData($workspaceId, [
-                'input' => $inputs,
+                'input' => $this->inputs,
             ]);
             $this->logger->info('All files were fetched.');
         }
