b39461dd70bc29a4d632ae12900bb9a67a37acd3
Vladimír Kriška <vlado@rekurzia.com>
2020-12-09 17:48:53 +0100

    Add BuildQueryFromConfigurationHelper and tests

---
+++ Reader/Helper/BuildQueryFromConfigurationHelper.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Keboola\InputMapping\Reader\Helper;
+
+class BuildQueryFromConfigurationHelper
+{
+    public static function buildQuery($configuration)
+    {
+        if (isset($configuration['query']) && isset($configuration['source']['tags'])) {
+            return sprintf(
+                '%s AND (%s)',
+                $configuration['query'],
+                self::buildQueryForTags(
+                    self::getTagsFromSourceTags($configuration['source']['tags'])
+                )
+            );
+        }
+        if (isset($configuration['source']['tags'])) {
+            return self::buildQueryForTags(
+                self::getTagsFromSourceTags($configuration['source']['tags'])
+            );
+        }
+        return $configuration['query'];
+    }
+
+    public static function buildQueryForTags($tags)
+    {
+        return implode(
+            ' AND ',
+            array_map(function ($tag) {
+                return sprintf('tags:"%s"', $tag);
+            }, $tags)
+        );
+    }
+
+    public static function getTagsFromSourceTags($tags)
+    {
+        return array_map(function ($tag) {
+            return $tag['name'];
+        }, $tags);
+    }
+}
