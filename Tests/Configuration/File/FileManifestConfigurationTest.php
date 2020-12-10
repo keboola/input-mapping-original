e2ef7dad97b2a93a752a9d8563993e15158183ec
Vladimír Kriška <vlado@keboola.com>
2020-12-10 23:02:22 +0100

    Revert "Support source.tags in configuration"
    This reverts commit 804501f72879ac38768a6ee01d0668e52d39dfff.

--- Tests/Configuration/File/FileManifestConfigurationTest.php
+++ Tests/Configuration/File/FileManifestConfigurationTest.php
@@ -24,34 +24,6 @@ class FileManifestConfigurationTest extends \PHPUnit_Framework_TestCase
         self::assertEquals($expectedResponse, $processedConfiguration);
     }
 
-    public function testConfigurationWithSourceTags()
-    {
-        $config = [
-            "id" => 1,
-            "name" => "test",
-            "created" => "2015-01-23T04:11:18+0100",
-            "is_public" => false,
-            "is_encrypted" => false,
-            "tags" => ["tag1", "tag2"],
-            "source" => [
-                "tags" => [
-                    [
-                        "name" => "tag1"
-                    ],
-                    [
-                        "name" => "tag2"
-                    ]
-                ]
-            ],
-            "max_age_days" => 180,
-            "size_bytes" => 4,
-            'is_sliced' => false,
-        ];
-        $expectedResponse = $config;
-        $processedConfiguration = (new Manifest())->parse(["config" => $config]);
-        self::assertEquals($expectedResponse, $processedConfiguration);
-    }
-
     /**
      * @expectedException \Symfony\Component\Config\Definition\Exception\InvalidConfigurationException
      * @expectedExceptionMessage The child node "id" at path "file" must be configured.
