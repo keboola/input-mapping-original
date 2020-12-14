c64434fc7eb7bfb49f9f0d9a9db9d75cc5ca4dfb
Erik Zigo <erik.zigo@keboola.com>
2020-12-14 15:11:00 +0100

    Reader::getFiles - Restrict processed_tags for dev branch

--- Reader/Reader.php
+++ Reader/Reader.php
@@ -190,6 +190,10 @@ class Reader
             throw new InvalidInputException("Invalid file mapping, 'query' attribute is restricted for dev/branch context.");
         }
 
+        if (isset($fileConfiguration["processed_tags"]) && $clientWrapper->hasBranch()) {
+            throw new InvalidInputException("Invalid file mapping, 'processed_tags' attribute is restricted for dev/branch context.");
+        }
+
         $options = new ListFilesOptions();
         if (empty($fileConfiguration['tags']) && empty($fileConfiguration['query'])) {
             throw new InvalidInputException("Invalid file mapping, both 'tags' and 'query' are empty.");
