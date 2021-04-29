f585f2351dcd212740bc101fffdfabcc00c35b97
pivnicek <marc@keboola.com>
2021-04-29 13:42:56 +0200

    add FileState classes

---
+++ src/State/InputFileStateList.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Keboola\InputMapping\State;
+
+use JsonSerializable;
+use Keboola\InputMapping\Exception\FileNotFoundException;
+
+class InputFileStateList implements JsonSerializable
+{
+    /**
+     * @var InputFileState[]
+     */
+    private $files = [];
+
+    public function __construct(array $configurations)
+    {
+        foreach ($configurations as $item) {
+            $this->files[] = new InputTableState($item);
+        }
+    }
+
+    /**
+     * @param $fileTags
+     * @return InputFileState
+     * @throws FileNotFoundException
+     */
+    public function getFile($fileTags)
+    {
+        foreach ($this->files as $file) {
+            if ($file->getTags() === $fileTags) {
+                return $file;
+            }
+        }
+        throw new FileNotFoundException('State for files defined by "' . implode(', ', $fileTags) . '" not found.');
+    }
+
+    /**
+     * @return array
+     */
+    public function jsonSerialize()
+    {
+        return array_map(function (InputFileState $file) {
+            return $file->jsonSerialize();
+        }, $this->files);
+    }
+}
