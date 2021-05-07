0c44c1f7c6927e9720c2da377cd8aacfb9ceea6e
pivnicek <marc@keboola.com>
2021-05-07 14:29:08 +0200

    add changed_since to query

--- src/Helper/BuildQueryFromConfigurationHelper.php
+++ src/Helper/BuildQueryFromConfigurationHelper.php
@@ -2,21 +2,30 @@
 
 namespace Keboola\InputMapping\Helper;
 
+use Keboola\InputMapping\Table\Options\InputTableOptions;
+
 class BuildQueryFromConfigurationHelper
 {
     public static function buildQuery($configuration)
     {
+        $query = $configuration['query'];
         if (isset($configuration['query']) && isset($configuration['source']['tags'])) {
-            return sprintf(
+            $query = sprintf(
                 '%s AND (%s)',
                 $configuration['query'],
                 self::buildQueryForSourceTags($configuration['source']['tags'])
             );
+        } elseif (isset($configuration['source']['tags'])) {
+            $query = self::buildQueryForSourceTags($configuration['source']['tags']);
         }
-        if (isset($configuration['source']['tags'])) {
-            return self::buildQueryForSourceTags($configuration['source']['tags']);
+        if (isset($configuration['changed_since'])
+            && $configuration['changed_since'] !== InputTableOptions::ADAPTIVE_INPUT_MAPPING_VALUE) {
+            $query .= sprintf(
+                ' AND created:>%s',
+                date('Y-m-d H:i:s', strtotime($configuration['changed_since']))
+            );
         }
-        return $configuration['query'];
+        return $query;
     }
 
     public static function buildQueryForSourceTags(array $tags)
