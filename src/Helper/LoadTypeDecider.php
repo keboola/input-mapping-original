2d70feb45cc20f22ca375dd026a3d1217a5d6e4b
odin <odinuv@gmail.com>
2021-07-22 17:43:58 +0200

    Don't use clone loading when filtered alias is used https://keboola.atlassian.net/browse/PS-2378

--- src/Helper/LoadTypeDecider.php
+++ src/Helper/LoadTypeDecider.php
@@ -6,6 +6,10 @@ class LoadTypeDecider
 {
     public static function canClone(array $tableInfo, $workspaceType, array $exportOptions)
     {
+        if ($tableInfo['isAlias'] && (empty($tableInfo['aliasColumnsAutoSync']) || !empty($tableInfo['aliasFilter']))) {
+            return false;
+        }
+
         if (array_keys($exportOptions) !== ['overwrite'] || ($tableInfo['bucket']['backend'] !== $workspaceType) || ($workspaceType !== 'snowflake')) {
             return false;
         }
